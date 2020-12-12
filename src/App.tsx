682f80553b11dab1567f84e249d82b34bd4caea7
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-12-12 22:55:32 +0700

    Update UI and add SEO

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import { SheetPage } from './components/sheet/SheetPage';
 export const App = () => {
   return (
     <div className="App">
-      <header><h1>Learn Music Sight Reading</h1></header>
+      <header>
+        <h1>Learn Music Sight Reading</h1>
+      </header>
       <main>
         <SheetPage sheetHandler={new SheetHandler()} />
       </main>
